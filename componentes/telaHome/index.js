0d6720bcafd1a8a276eb7658ac63e6feca604fca
LzynBSK <leonardovitoriano203@gmail.com>
2023-03-29 15:21:05 -0300

    Tela Login Roxa

--- componentes/telaHome/index.js
+++ componentes/telaHome/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { TextInput, Text, View, Button, SafeAreaView, StyleSheet, onChangeText, TouchableOpacity } from 'react-native';
 
 import estilo from './estilo';
@@ -8,6 +8,7 @@ import estilo from './estilo';
   export default function Home (props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+      
 
     const handleLogin = () => {
       // lógica de login aqui
@@ -21,13 +22,13 @@ import estilo from './estilo';
       Nome do Usuário
       </Text>
       <TextInput
-        style={styles.input}
+        style={styles.input1}
         placeholder="Usuário"
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
-        style={styles.input}
+        style={styles.input2}
         placeholder="Senha"
         value={password}
         onChangeText={setPassword}
@@ -42,28 +43,51 @@ import estilo from './estilo';
 );
 };
 const styles = StyleSheet.create({
-  input: {
+  input1: {
     height: 40,
-    margin: 12,
+    marginLeft: 7,
+    marginRight: 7,
     borderWidth: 1,
+    borderColor: "#813BF5",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
     padding: 10,
+    width: '90%'
+  },
+  input2: {
+    height: 40,
+    marginTop: -1,
+    marginLeft: 7,
+    marginRight: 7,
+    borderWidth: 1,
+    borderColor: "#813BF5",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    padding: 10,
+    width: '90%'
   },
 
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   txtCadastro:{
     fontSize: 16,
     fontWeight: 'bold',
-    marginLeft: 15
+    alignSelf: "center",
+    marginBottom: 10
   },
   enterButton:{
     height: 35, 
     marginTop: 5,
-    marginLeft: 310,
+    marginLeft: 7,
+    marginRight: 7,
     alignItems: 'center',
-    flexDirection: 'row',
     justifyContent: 'center',
     backgroundColor:'#AB76FA',
-    borderRadius:   10,
-    width: 80,
-    
+    borderRadius: 10,
+    width: 379,
+    width: '90%'
   }
 });
